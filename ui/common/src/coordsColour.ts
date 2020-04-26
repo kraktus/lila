e3c7aefff51d00ebaade0ad947ad5fbfe0591dc1
kraktus <56031107+kraktus@users.noreply.github.com>
2020-04-26 07:58:45 +0100

    Rewrite ChangeColour

--- ui/common/src/coordsColour.ts
+++ ui/common/src/coordsColour.ts
@@ -1,4 +1,6 @@
-export function changeColour(theme: string){
+export default function changeColourHandle(){
+
+	const list_class: string[] = $('body').attr('class').split(' ')
 
 	const dict: {[color: string]: [string, string]} = {
 		"blue": ["#DEE3E6", "#788a94"],
@@ -9,9 +11,12 @@ export function changeColour(theme: string){
 		"brown": ["#F0D9B5", "#946f51"],
 		"pink": ["#f1f1c9", "#f07272"]
 	}
-	if (theme in dict){
-		document.documentElement.style.setProperty('--color-white', dict[theme][0]);
-		document.documentElement.style.setProperty('--color-black', dict[theme][1]);
-		document.documentElement.style.setProperty('--shadow', "none");
+	let color: string
+	for (color of list_class){
+		if (color in dict){
+			document.documentElement.style.setProperty('--color-white', dict[color][0]);
+			document.documentElement.style.setProperty('--color-black', dict[color][1]);
+			document.documentElement.style.setProperty('--shadow', "none");
+		}
 	}
 }
