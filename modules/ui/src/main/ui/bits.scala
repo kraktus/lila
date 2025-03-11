bf3a4cb2906ace307623a8ea4b6963ca88ce7fd1
kraktus <kraktus@users.noreply.github.com>
2025-03-11 10:29:26 +0100

    Turn dateform with mselect into a component

--- modules/ui/src/main/ui/bits.scala
+++ modules/ui/src/main/ui/bits.scala
@@ -11,7 +11,6 @@ import lila.core.security.HcaptchaForm
 
 import ScalatagsTemplate.{ *, given }
 
-
 object bits:
 
   val engineFullName = "Stockfish 17"
@@ -34,41 +33,43 @@ object bits:
       st.nav(cls := "mselect__list")(items.map(_(cls := "mselect__item")))
     )
 
-
-  def calendarMselect(id: String, allYears: List[Int], url:(Int,Month) => play.api.mvc.Call)(at: YearMonth)(using lang: Lang) = 
-    def prefix(suffix: String) = s"calendar-mselect__form$suffix"
+  def calendarMselect(id: String, allYears: List[Int], url: (Int, Month) => play.api.mvc.Call)(at: YearMonth)(
+      using lang: Lang
+  ) =
+    val prefix                   = s"calendar-mselect"
+    def prefixed(suffix: String) = s"${prefix}$suffix"
     // TODO FIXME how to access DateHelper.showMonth
     def showMonth(m: Month) = m.toString
-      div(cls := prefix(s"--$id"))(
-          a(
-            href     := url(at.minusMonths(1).getYear, at.minusMonths(1).getMonth),
-            dataIcon := Icon.LessThan
-          ),
-          div(cls := prefix("__selects"))(
-            mselect(
-              prefix(s"__year--$id"),
-              span(at.getYear),
-              allYears.map: y =>
-                a(
-                  cls  := (y == at.getYear).option("current"),
-                  href := url(y, at.getMonth)
-                )(y)
-            ),
-            mselect(
-              prefix(s"__month--$id"),
-              span(showMonth(at.getMonth)),
-              java.time.Month.values.toIndexedSeq.map: m =>
-                a(
-                  cls  := (m == at.getMonth).option("current"),
-                  href := url(at.getYear, m)
-                )(showMonth(m))
-            )
-          ),
-          a(
-            href     := url(at.plusMonths(1).getYear, at.plusMonths(1).getMonth),
-            dataIcon := Icon.GreaterThan
-          )
+    div(cls := s"$prefix $prefix--$id")(
+      a(
+        href     := url(at.minusMonths(1).getYear, at.minusMonths(1).getMonth),
+        dataIcon := Icon.LessThan
+      ),
+      div(cls := prefixed("__selects"))(
+        mselect(
+          prefixed(s"__year--$id"),
+          span(at.getYear),
+          allYears.map: y =>
+            a(
+              cls  := (y == at.getYear).option("current"),
+              href := url(y, at.getMonth)
+            )(y)
+        ),
+        mselect(
+          prefixed(s"__month--$id"),
+          span(showMonth(at.getMonth)),
+          java.time.Month.values.toIndexedSeq.map: m =>
+            a(
+              cls  := (m == at.getMonth).option("current"),
+              href := url(at.getYear, m)
+            )(showMonth(m))
         )
+      ),
+      a(
+        href     := url(at.plusMonths(1).getYear, at.plusMonths(1).getMonth),
+        dataIcon := Icon.GreaterThan
+      )
+    )
 
   def fenAnalysisLink(fen: Fen.Full)(using Translate) =
     a(href := routes.UserAnalysis.parseArg(ChessHelper.underscoreFen(fen)))(
